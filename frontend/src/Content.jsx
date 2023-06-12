11f467a4323d3f27efacfbdcfeb2638b7f0e8d28
Savraj <savrajcheema@hotmail.com>
2023-06-12 01:26:47 -0700

    feat: can only save once every 30 seconds now" git commit -m " feat: can only save once every thirty seconds now"
    "

--- frontend/src/Content.jsx
+++ frontend/src/Content.jsx
@@ -1,20 +1,28 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const Content = ({ content, time, title,noteID,tags }) => {
+const Content = ({ content, time, title, noteID, tags }) => {
+  console.log("Content pagenoteTitle: " + title);
+  console.log("Content page noteid: " + noteID);
+  console.log("Content page tags: " + tags);
+  console.log("Content page noteContenr: " + content);
   // Convert epoch time to a Date object
   const formattedTime = new Date(time).toLocaleString();
 
-  // State variables to track the edited title and content
+  // State variables to track the edited title, content, and tags
   const [editedTitle, setEditedTitle] = useState("");
   const [editedContent, setEditedContent] = useState("");
+  const [editedTags, setEditedTags] = useState([]);
+  const [isSaving, setIsSaving] = useState(false);
 
-  // Update the editedTitle state when the title prop changes
+  // Update the editedTitle, editedContent, and editedTags state when the respective props change
   useEffect(() => {
     setEditedTitle(title);
-  }, [title]);
+    setEditedContent(content);
+    setEditedTags(tags);
+  }, [title, content, tags]);
 
-  // Event handlers for title and content change
+  // Event handlers for title, content, and tags change
   const handleTitleChange = (e) => {
     setEditedTitle(e.target.value);
   };
@@ -23,28 +31,69 @@ const Content = ({ content, time, title,noteID,tags }) => {
     setEditedContent(e.target.value);
   };
 
-  // Event handler for saving the data
-  const handleSave = () => {
-    console.log("Content pagenoteTitle: " + editedTitle)
-    console.log("Content page noteid: " + noteID)
-    console.log("Content page tags: " + tags)
-    console.log("Content page noteContenr: " + editedContent)
-    // Create the data object to be sent in the API request
-    const data = {
-      noteTitle: editedTitle,
-      noteContent: editedContent,
-      tags: tags,
-    };
+  const handleTagChange = (e) => {
+    setEditedTags(e.target.value.split(","));
+  };
 
-    // Make the API request using Axios
-    axios
-      .post(`https://l7flqpsmca.execute-api.us-west-2.amazonaws.com/user/1/note/${noteID}`, data)
-      .then((response) => {
-        console.log("Data saved successfully:", response.data);
-      })
-      .catch((error) => {
-        console.error("Error saving data:", error);
-      });
+  // Event handler for saving the data
+  // Event handler for saving the data
+const handleSave = () => {
+    if (!isSaving) {
+      setIsSaving(true);
+  
+      console.log("Content pagenoteTitle: " + editedTitle);
+      console.log("Content page noteid: " + noteID);
+      console.log("Content page tags: " + editedTags);
+      console.log("Content page noteContenr: " + editedContent);
+  
+      // Create the data object to be sent in the API request
+      const data = {
+        noteTitle: editedTitle,
+        noteContent: editedContent,
+        tags: editedTags,
+      };
+  
+      // Make the API request using Axios
+      axios
+        .post(
+          `https://l7flqpsmca.execute-api.us-west-2.amazonaws.com/user/1/note/${noteID}`,
+          data
+        )
+        .then((response) => {
+          console.log("Data saved successfully:", response.data);
+          setTimeout(() => {
+            setIsSaving(false);
+          }, 30000); // Allow saving again after 30 seconds
+        })
+        .catch((error) => {
+          console.error("Error saving data:", error);
+          setIsSaving(false);
+        });
+    }
+  };
+  
+
+
+  // Save the content when the component is unmounted
+  useEffect(() => {
+    return () => {
+      handleSave();
+    };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  // Event handler for Enter key press
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      if (!isSaving) {
+        setIsSaving(true);
+        handleSave();
+        setTimeout(() => {
+          setIsSaving(false);
+        }, 30000); // Allow saving again after 30 seconds
+      }
+    }
   };
 
   return (
@@ -56,19 +105,32 @@ const Content = ({ content, time, title,noteID,tags }) => {
           onChange={handleTitleChange}
           className="title-input"
         />
-        <button className="note-submit" onClick={handleSave}>
+        <label htmlFor="tagsInput" className="tags-title">Tags: </label>
+        <input
+        className="tags-input"
+          type="text"
+          id="tagsInput"
+          value={editedTags.join(",")}
+          onChange={handleTagChange}
+        />
+        <button
+          className="note-submit"
+          onClick={handleSave}
+          disabled={isSaving}
+        >
           Save
         </button>
       </h2>
       <textarea
         className="content-textarea"
-        defaultValue={content}
+        value={editedContent}
         onChange={handleContentChange}
+        onKeyPress={handleKeyPress}
       ></textarea>
       <h3 className="timeDisplay">{formattedTime}</h3>
     </div>
   );
+  
 };
 
 export default Content;
-
